955c3c6d4c2db4ab20239eab19c091e8898508ff
PiyushVerma4747 <pv839238@gmail.com>
2026-08-14 09:03:32 +0000

    Sqrt

---
+++ ARRAYS/EAZY/LEET_69.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+    int lo=1;
+    int hi=x;
+    if(x==0) return 0;
+            while(lo<=hi){
+                int mid=(lo+hi)/2;
+                if(mid==x/mid) return mid;
+                else if(mid>x/mid) hi=mid-1;
+                else lo=mid+1;
+        }
+    return hi; 
+    }
+}
