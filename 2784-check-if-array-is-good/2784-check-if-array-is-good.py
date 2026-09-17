7dce915db1a69ca0e618fb4c5188b29d18e7b7c7
PiyushVerma4747 <pv839238@gmail.com>
2026-09-18 01:41:00 +0530

    Time: 0 ms (100%), Space: 19.3 MB (63.02%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,22 +1,39 @@
+# class Solution:
+#     def isGood(self, nums: List[int]) -> bool:
+#         n=max(nums)
+#         dic={}
+#         if n + 1 !=len(nums):
+#             return False
+#         for i in nums:
+#             if i in dic:
+#                 dic[i]+=1
+#             else:
+#                 dic[i]=1
+#         for i in dic:
+#             if dic[i]>=2 and i!=n:
+#                 return False
+#         req=n*(n+1)//2 + n
+#         summ=sum(nums)
+#         print(req  , summ)
+#         if summ == req:
+#             return True
+#         else:
+#             return False
+        
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        n=max(nums)
-        dic={}
-        if n + 1 !=len(nums):
+        n = max(nums)
+
+        if len(nums) != n + 1:
             return False
+
+        freq = [0] * (n + 1)
+
         for i in nums:
-            if i in dic:
-                dic[i]+=1
-            else:
-                dic[i]=1
-        for i in dic:
-            if dic[i]>=2 and i!=n:
+            freq[i] += 1
+
+        for i in range(1, n):
+            if freq[i] != 1:
                 return False
-        req=n*(n+1)//2 + n
-        summ=sum(nums)
-        print(req  , summ)
-        if summ == req:
-            return True
-        else:
-            return False
-        
+
+        return freq[n] == 2
